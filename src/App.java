1d531e1f918021db53ffadbab1a5d7acc3dc883d
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-06-14 20:46:33 -0500

    setup for a2z dsa

---
+++ src/App.java
@@ -0,0 +1,11 @@
+package src;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Hello," + x);
+        sc.close();
+    }
+}
